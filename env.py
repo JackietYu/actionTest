4ec959535003468bcfb4b747d736376336e97d5c
JackYu <34149758+JackietYu@users.noreply.github.com>
2020-08-31 21:43:40 +0800

    Update env.py

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 #注意：.env中变量的写法一般是 TEST_KEY = TEST_VALUE。最好去除每个字符两边的空格，否则加到环境变量里也读取不到
 import os
 
-print(os.environ['MY_HUB'])
+#print(os.environ['MY_HUB'])
 hub1 = os.getenv('MY_HUB')
 hub2 = os.environ.get('MY_HUB')
 print(hub1)
